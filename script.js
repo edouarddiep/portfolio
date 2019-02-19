5483c8bbcd30211f32f6b8aa1111baea96658358
eddydiep <edouard.diep@gmail.com>
2019-02-19 14:36:25 +0100

    V3.0 February revisited

--- script.js
+++ script.js
@@ -7,39 +7,50 @@
 
 $(document).ready(function(){
 
-     // action when refreshing html page
-    $(this).scrollTop(0);
-    
-  // typing animation
-  (function($) {
-    $.fn.delay(3000).writeText = function(content) {
-        var contentArray = content.split(""),
-            current = 0,
-            elem = this;
-        setInterval(function() {
-            if(current < contentArray.length) {
-                elem.text(elem.text() + contentArray[current++]);
-            }
-        }, 80);
-    };
-    
-  })(jQuery);
-
-
-  // input text for typing animation 
-  $("#titleHolder").writeText("Hello ! I am Edouard");
-
-
-  // input text for typing animation 
-  setTimeout(function(){
-    $("#pHolder").delay(200).writeText("A Full Stack developer");
-  }, 1500);
-
-
     // main variables
     var dev = $('.development-wrapper');
     var width = $(window).width();
     var lineProgressVisible = false;
+    var d = new Date();
+    var n = d.getHours();
+
+    // action when refreshing html page
+$(this).scrollTop(0);
+
+// typing animation
+(function($) {
+$.fn.delay(1000).writeText = function(content) {
+    var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+    setInterval(function() {
+        if(current < contentArray.length) {
+            elem.text(elem.text() + contentArray[current++]);
+        }
+    }, 60);
+};
+
+})(jQuery);
+
+
+// input text for typing animation 
+if(n<18){
+    $("#first-part-writer").writeText("Bonjour !");
+} else {
+    $("#first-part-writer").writeText("Bonsoir !");
+}
+
+setTimeout(function(){
+    $("#sec-part-writer").delay(600).writeText("Je suis Edouard");
+}, 600);
+
+
+// input text for typing animation 
+setTimeout(function(){
+    $("#last-part-writer").delay(1000).writeText("Un développeur Full Stack");
+}, 2000);
+
+
 
 
     // logo loading animation
@@ -62,40 +73,47 @@ $(document).ready(function(){
     $('.navbar ul').delay(300).animate({
         'opacity':'1',
         'top': '0',
-        'left':'0'
-    },800);
+        'left':'0',
+        'zoom':'0.98'
+    },1000);
 
 
     // welcome loading animation
-    $('.mainPage .content .welcome-wrapper').animate({
+    $('.mainPage .content .last-part-writer').delay(1700).animate({
         'opacity':'1'
-    },500);
-   
+    },700);
+
+    // objectif wrapper animation
+    $('.mainPage .content .description-wrapper .objectif-wrapper').delay(3500).animate({
+        'opacity':'1',
+        'padding-top':'25px'
+    },1500);
 
     /****** SCROLLING ANIMATION WITH CLICK ON NAVBAR ******/
 
     //store internal links in a variable
     var $clickedNavLinks = $('a[href^="#"]');
     //smooth scrolling on clicking navigation link
-    if(width >= 768){
+    if(width >= 812){
         $clickedNavLinks.on('click', function(e) {
             //find the links and the clicked link
-            var target = this.hash,
-                    $target = $(target),
-                    $navLinks = $('.navbar a');
+            var offset = 200
+
             //stop default link behaviour
+            e.stopImmediatePropagation();
             e.preventDefault();
             //do the scrolling
             $('html, body').stop().animate({
-                'scrollTop': $target.offset().top - $target.outerHeight()/6
-            }, 1200, 'swing');
+                'scrollTop': $(this.hash).offset().top - offset
+            }, 1200, 'swing', function(){
+                window.location.hash = this.hash;
+            });
         });
     } else {
         $clickedNavLinks.on('touchstart mouseenter focus', function(e) {
             //find the links and the clicked link
-            var target = this.hash,
-                    $target = $(target),
-                    $navLinks = $('.navbar a');
+            var offset = 50
+
             //stop default link behaviour
             if(e.type == 'touchstart'){
                 e.stopImmediatePropagation();
@@ -103,13 +121,14 @@ $(document).ready(function(){
             }
             //do the scrolling
             $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, 1200, 'swing');
+                'scrollTop': $(this.hash).offset().top - offset
+            }, 1200, 'swing', function(){
+                window.location.hash = this.hash;
+            });
         });
     }
 
     $('body').scroll(function(){
-       
         var bottom_of_window = $(this).scrollTop() + $(this).outerHeight()/2.5;
 
         // SECTION EXPERIENCE
@@ -118,52 +137,47 @@ $(document).ready(function(){
 
             var bottom_of_object = $(this).offset().top - $(this).outerHeight();
 
-            console.log("BOTTOM OF WINDOW = "+bottom_of_window);
-            console.log("BOTTOM OF EXPERIENCE = "+bottom_of_object);
-
             /* If the object is completely visible in the window, fadeIn it */
-            
-          
-            if( bottom_of_window > bottom_of_object){
-              $(this).animate({
-                'opacity':'1',
-                'margin-left': '0',
-              },1000);
-            }
-        });
-  
-        // SECTION SKILLS IT
-
-        var middle_dev_scroll = dev.offset().top - dev.outerHeight()/2.5;
-        console.log("BOTTOM OF SKILLS = "+middle_dev_scroll);
-
-
-        if((bottom_of_window > middle_dev_scroll) && lineProgressVisible == false){
-            // Managing bars line load
-            $('.skills-bar-container li').each( function(){
-                var $barContainer = $(this).find('.bar-container');
-                var dataPercent = parseInt($barContainer.data('percent'));
-                var elem = $(this).find('.progressbar');
-                var percent = $(this).find('.percent');
-                var width = 0;
         
-                var id = setInterval(frame, 30);
-        
-                function frame() {
-                  if (width >= dataPercent) {
-                      clearInterval(id);
-                  } else {
-                    width++;
-                    elem.css("width", width+"%");
-                    percent.html(width+" %");
-                  }
-                }
-              });
-              lineProgressVisible = true;
+        if( bottom_of_window > bottom_of_object){
+            $(this).animate({
+            'opacity':'1',
+            'margin-left': '0',
+            },1000);
         }
+    });
 
-        $('.img-portfolio').each(function(){
- 
+    // SECTION SKILLS IT
+
+    var middle_dev_scroll = dev.offset().top - dev.outerHeight()/2.5;
+
+
+    if((bottom_of_window > middle_dev_scroll) && lineProgressVisible == false){
+        // Managing bars line load
+        $('.skills-bar-container li').each( function(){
+            var $barContainer = $(this).find('.bar-container');
+            var dataPercent = parseInt($barContainer.data('percent'));
+            var elem = $(this).find('.progressbar');
+            var percent = $(this).find('.percent');
+            var width = 0;
+    
+            var id = setInterval(frame, 30);
+    
+            function frame() {
+                if (width >= dataPercent) {
+                    clearInterval(id);
+                } else {
+                width++;
+                elem.css("width", width+"%");
+                percent.html(width+" %");
+                }
+            }
+            });
+            lineProgressVisible = true;
+    }
+
+    $('.img-portfolio').each(function(){
+
             if(bottom_of_window > middle_dev_scroll){
                 if(width > 1024){
                     $(this).animate({
